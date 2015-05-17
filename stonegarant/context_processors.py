6616239fb40aa972d76e5e06dfc8767b8ff8afef
pomahtuk <pman89@ya.ru>
2015-05-17 22:56:43 +0200

    catalog markup progress

--- stonegarant/context_processors.py
+++ stonegarant/context_processors.py
@@ -3,4 +3,7 @@ from django.conf import settings # import the settings file
 
 def jivosite(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
-    return {'JIVOSITE_ID': settings.JIVOSITE_ID}
+    return {
+        'JIVOSITE_ID': settings.JIVOSITE_ID,
+        'STATIC_URL': settings.STATIC_URL
+    }
